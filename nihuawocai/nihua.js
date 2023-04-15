6f24b41e54fa566d1a98b1df8fabea619b8cd8c4
buzhidao <709798511@qq.com>
2023-04-15 23:48:41 +0800

    不懂了

--- nihuawocai/nihua.js
+++ nihuawocai/nihua.js
@@ -20,16 +20,40 @@ const ws = require('ws');
     function handleError(){
         console.log('WebSocket error');
     }
+    var host = ""
     function handleConnection(ws){
         console.log('WebSocket connection');
         ws.on('message',handleMessage);
     }
     function handleMessage(msg){
         console.log('WebSocket message');
-        console.log(msg);
-        server.clients.forEach(function(c){
-            c.send(msg);
-        })
+        console.log(msg.toString('utf8'));
+        var json1 = JSON.parse(msg.toString('utf8'))
+        switch(json1.type){
+            case "message":
+                server.clients.forEach(function(c){
+                    c.send(msg);
+                })
+                break
+            case "start":
+                console.log("start")
+                var renshu = server.clients.size
+                server.clients.forEach(function(c){
+                    c.send(msg);
+                })
+                break
+            case "username":
+                if(host == ""){
+                    host = json1.user
+                    console.log("host == " + host)
+                }
+                if(json1.user == host){
+                    server.clients.forEach(function(c){
+                        c.send(msg);
+                    })
+                }
+                break
+        }
     }
     init();
 })(ws);
