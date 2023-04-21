cfc1a962976e5e5ed1d6498e409b89999ea22fd8
buzhidao <709798511@qq.com>
2023-04-21 23:19:31 +0800

    基本功能

--- nihuawocai/nihua.js
+++ nihuawocai/nihua.js
@@ -8,8 +8,8 @@ const ws = require('ws');
     var renshu = 0
     var uidlist = []
     var wanjia = []
-    var dengdai = []
-    var agreenumber
+    var dengdairenshu = 0
+    var kaishijishu = 0
     var wancheng = 0
     var huihe = 1
     var shunxu = [[]]
@@ -17,6 +17,8 @@ const ws = require('ws');
     var ren = [[]]
     var zonghuihe = 0
     var isingame = false
+    var host = null
+    var timeout
 
     function bindEvent() {
         server.on('open',handleOpen);
@@ -61,132 +63,168 @@ const ws = require('ws');
             case "message":
                 sendbuf(msg)
                 break
-            case "start":
-                console.log("start")
-                console.log("renshu == " + renshu)
-                isingame = true
-                wancheng = 0
-                if(json1.zonghuihe == "" || json1.zonghuihe.zonghuihe == NaN){
-                    zonghuihe = renshu
-                }else if(typeof(json1.zonghuihe) == 'number'){
+            case "askforstart":
+                sendbuf(msg)
+                kaishijishu = 0
+                dengdairenshu = 0
+                wanjia = []
+                console.log("that zonghuihe == " + json1.zonghuihe)
+                if(json1.zonghuihe == 0 || json1.zonghuihe.zonghuihe == NaN){
+                    zonghuihe = server.clients.size
+                }else{
                     zonghuihe = json1.zonghuihe
                 }
+                console.log("this zonghuihe == " + zonghuihe)
                 zongshijian = json1.shijianbeilv * 150
-                console.log("zonghuihe == " + zonghuihe)
-                console.log("zongshijian == " + zongshijian)
-                huihe = 1
-                hua = [[]]
-                shunxu = [[]]
-                for(i = 0; i < zonghuihe; i++){
-                    shunxu[i] = []
-                    for(j = 0; j < renshu; j++){
-                        shunxu[i][j] = (i + j) % renshu
-                    }
+                break
+            case "start":
+                renshu = server.clients.size
+                console.log("renshu == " + renshu)
+                if(json1.user != null){
+                    wanjia[kaishijishu] = json1.user
+                    kaishijishu += 1
+                    console.log(kaishijishu)
                 }
-                for(i = 0; i < 30; i++){
-                    
-                    if(i % 2 == 0){
-                        var r1 = randbelow(zonghuihe)
-                        var r2 = randbelow(zonghuihe - 1)
-                        if(r2 == r1) r2 = zonghuihe - 1
-                        var li = shunxu[r1]
-                        shunxu[r1] = shunxu[r2]
-                        shunxu[r2] = li
-                    } else{
-                        var r1 = randbelow(renshu)
-                        var r2 = randbelow(renshu - 1)
-                        if(r2 == r1) r2 = renshu - 1
-                        for(j = 0; j < zonghuihe; j++){
-                            var li = shunxu[j][r1]
-                            shunxu[j][r1] = shunxu[j][r2]
-                            shunxu[j][r2] = li
+                if(kaishijishu == renshu){
+                    console.log("start")
+                    isingame = true
+                    wancheng = 0
+                    console.log("zonghuihe == " + zonghuihe)
+                    console.log("zongshijian == " + zongshijian)
+                    huihe = 1
+                    hua = [[]]
+                    shunxu = [[]]
+                    for(i = 0; i < zonghuihe; i++){
+                        shunxu[i] = []
+                        for(j = 0; j < renshu; j++){
+                            shunxu[i][j] = (i + j) % renshu
                         }
                     }
-                    
-                }
-                json1.message = shunxu
-                json1.zongshijian = zongshijian
-                console.log(json1)
-                sendjson(json1)
-                break
-            case "username":
-                if(isingame){
-                    json1.type = "askforin"
-                    sendjson(json1)
-                }else{
-                    renshu += 1
-                    wanjia.push(json1.user)
-                    var newuid
-                    if (uidlist.length == 0){
-                        newuid = 0
-                    }else{
-                        newuid = uidlist.length
-                        for(i = 0; i < uidlist.length; i++){
-                            if(uidlist[i] == null){
-                                newuid = i
+                    if(renshu == zonghuihe){
+                        for(i = 0; i < 30; i++){
+                            if(i % 2 == 0){
+                                var r1 = randbelow(zonghuihe)
+                                var r2 = randbelow(zonghuihe - 1)
+                                if(r2 == r1) r2 = zonghuihe - 1
+                                var li = shunxu[r1]
+                                shunxu[r1] = shunxu[r2]
+                                shunxu[r2] = li
+                            } else{
+                                var r1 = randbelow(renshu)
+                                var r2 = randbelow(renshu - 1)
+                                if(r2 == r1) r2 = renshu - 1
+                                for(j = 0; j < zonghuihe; j++){
+                                    var li = shunxu[j][r1]
+                                    shunxu[j][r1] = shunxu[j][r2]
+                                    shunxu[j][r2] = li
+                                }
                             }
                         }
                     }
                     
-                    json1.message = newuid
+                    json1.message = shunxu
+                    json1.zongshijian = zongshijian
+                    json1.wanjia = wanjia
+                    console.log(json1)
+                    sendjson(json1)
+                    timeout = setTimeout(() => {
+                        json1.type = "jieshuchuti"
+                        sendjson(json1)
+                    }, zongshijian * 0.3 * 1000);
+                }
+                
+                break
+            case "username":
+                
+                if(isingame){
+                    var newuid = null
+                    for(i = 0; i < wanjia.length; i++){
+                        if(wanjia[i] == json1.user){
+                            newuid = i
+                        }
+                    }
+                    if(newuid != null && renshu + dengdairenshu <= server.clients.size){
+                        json1.type = "backtogame"
+                        json1.uid = newuid
+                        json1.shunxu = shunxu
+                        json1.now = ["backtodraw","backtocai"][huihe % 2]
+                        json1.wanjia = wanjia
+                        uidlist[newuid] = newuid
+                        sendjson(json1)
+                    }else{
+                        dengdairenshu += 1
+                        json1.type = "dengdai"
+                        json1.wanjia = wanjia
+                        sendjson(json1)
+
+                    }
+
+                }else if (host == null){
+                    wanjia[wanjia.length] = json1.user
+                    host = json1.user
+                    json1.type = "join"
                     json1.wanjia = wanjia
                     uidlist[newuid] = newuid
                     sendjson(json1)
-                    if(renshu == 1){
-                        json1.type = "host"
-                        sendjson(json1)
-                    }
-                }
-                break
-            case "disagree":
-                sendjson(json1)
-                break
-            case "agree":
-                agreenumber += 1
-                if(agreenumber == renshu){
+                    json1.type = "host"
+                    json1.host = host
+                    sendjson(json1)
+                }else{
+                    wanjia[wanjia.length] = json1.user
+                    json1.type = "join"
+                    json1.wanjia = wanjia
+                    uidlist[newuid] = newuid
                     sendjson(json1)
                 }
-                break
-            case "exit":
-                renshu -= 1
-                uidlist[json1.uid] = null
-                wanjia.splice(wanjia.indexOf(json1.user), 1)
                 break
             case "chuti":
             case "huawanle":
             case "caice":
-                var weizhi = shunxu[huihe - 1].indexOf(json1.uid)
-                if(hua[huihe - 1] == undefined){
-                    hua[huihe - 1] = []
-                    ren[huihe - 1] = []
-                }
-                if(hua[huihe - 1][weizhi] == undefined){
-                    wancheng += 1
-                }
-                hua[huihe - 1][weizhi] = json1.message
-                ren[huihe - 1][weizhi] = json1.user
-                console.log(hua)
-                if(wancheng == renshu){
-                    if(huihe == zonghuihe){
-                        json1.type = "end"
-                        isingame = false
+                if(["huawanle","caice"][huihe % 2] == json1.type || ["huawanle","chuti"][huihe % 2] == json1.type){
+                    var weizhi = shunxu[huihe - 1].indexOf(json1.uid)
+                    if(hua[huihe - 1] == undefined){
+                        hua[huihe - 1] = []
+                        ren[huihe - 1] = []
+                    }
+                    if(hua[huihe - 1][weizhi] == undefined){
+                        wancheng += 1
+                    }
+                    hua[huihe - 1][weizhi] = json1.message
+                    ren[huihe - 1][weizhi] = json1.user
+                    //console.log(hua)
+                    if(wancheng >= server.clients.size - dengdairenshu){
+                        clearTimeout(timeout)
+                        if(huihe == zonghuihe){
+                            json1.type = "end"
+                            isingame = false
+                        }else{
+                            if (json1.type == "chuti" || json1.type == "caice"){
+                                json1.type = "draw"
+                                timeout = setTimeout(() => {
+                                    json1.type = "jieshuhuahua"
+                                    sendjson(json1)
+                                }, zongshijian * 1000);
+                            }else if (json1.type == "huawanle"){
+                                json1.type = "cai"
+                                timeout = setTimeout(() => {
+                                    json1.type = "jieshucaice"
+                                    sendjson(json1)
+                                }, zongshijian * 0.3 * 1000);
+                            }
+                        }    
+                        json1.ren = ren[huihe - 1]
+                        json1.message = [wancheng, server.clients.size, huihe - 1, weizhi, json1.message]
+                        wancheng = 0
+                        huihe += 1
+                        json1.huihe = huihe
                     }else{
-                        if (json1.type == "chuti" || json1.type == "caice"){
-                            json1.type = "draw"
-                        }else if (json1.type == "huawanle"){
-                            json1.type = "cai"
-                        }
-                    }    
-                    json1.message = hua[huihe - 1]
-                    json1.ren = ren[huihe - 1]
-                    wancheng = 0
-                    huihe += 1
-                    json1.huihe = huihe
-                }else{
-                    json1.message = [wancheng,renshu]
+                        json1.message = [wancheng, server.clients.size, huihe - 1, weizhi, json1.message]
+                    }
+                    
+                    sendjson(json1)
+                    break
                 }
-                sendjson(json1)
-                break
+                
         }
     }
     function randbelow(int){
